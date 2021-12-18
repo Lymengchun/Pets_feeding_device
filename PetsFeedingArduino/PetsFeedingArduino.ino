4231fe0d7deb240b289a4b716d2ad09ba02617ac
ngounMengleang <ngounmengleang18@gmail.com>
2021-12-18 09:50:03 +0700

    change load cell lib

--- PetsFeedingArduino/PetsFeedingArduino.ino
+++ PetsFeedingArduino/PetsFeedingArduino.ino
@@ -3,32 +3,20 @@
 #include <ArduinoJson.h>
 #include <FirebaseESP8266.h>
 #include <Servo.h>
-#include "HX711.h"
-#include <AverageValue.h>
-#define calibration_factor 7050.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
 
 #include <HX711_ADC.h>
 #if defined(ESP8266) || defined(ESP32) || defined(AVR)
 #include <EEPROM.h>
 #endif
 
-const int HX711_dout = 4; //mcu > HX711 dout pin
-const int HX711_sck = 0;  //mcu > HX711 sck pin
-
-HX711_ADC LoadCell(HX711_dout, HX711_sck);
-const int calVal_eepromAdress = 0;
-unsigned long t = 0;
-
-HX711 scale;
-float weight;
-long scaleActivateTime = 0;
-bool scaleActivate = false;
-AverageValue<float> averageValue(50);
-
+//dout , sck
+HX711_ADC FoodScale(10, 9);
+HX711_ADC PetScale(5, 16);
 int feednowID = 0;
 int feedAmount = 0;
-
 float oldFeed = 0.0;
+
+float previousWeight = 0.0;
 
 IPAddress local_ip(192, 168, 1, 1);
 IPAddress gateway(192, 168, 1, 1);
@@ -52,6 +40,7 @@ float floatValue;
 bool signupOK = false;
 int dataChange = 0;
 unsigned long servoMillis = 0;
+
 void scanAP()
 {
   int number = WiFi.scanNetworks();
@@ -121,6 +110,7 @@ void adddUserUUID()
   serializeJson(doc, output);
   server.send(200, "appication/json", output);
 }
+
 void setup()
 {
   Serial.begin(115200);
@@ -128,7 +118,6 @@ void setup()
   config.api_key = "AIzaSyB-PsV7BAiunqTRB8eTyL_mJxDJoLh2218";
   config.database_url = "https://pets-feeding-backend-default-rtdb.asia-southeast1.firebasedatabase.app/";
 
-  //Setup Servo
   myServo.attach(2);
   if (!WiFi.isConnected())
   {
@@ -142,49 +131,48 @@ void setup()
     server.begin();
   }
 
-  //  Serial.println("HX711 scale demo");
-
-  //  scale.begin(14, 12);
-  //  scale.set_scale(-7050.0); //This value is obtained by using the SparkFun_HX711_Calibration sketch
-  //  scale.tare();             //Assuming there is no weight on the scale at start up, reset the scale to 0
-  //
-  //  Serial.println("Readings:");
-
   servoMillis = millis();
   Serial.begin(115200);
-  Serial.println("HX711 scale demo");
-  //dout , sck
-  scale.begin(5, 16);
-  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
-  scale.tare();                        //Assuming there is no weight on the scale at start up, reset the scale to 0
+  Serial.println("Device Start");
 
   //Feed scale
-  LoadCell.begin();
-  float calibrationValue = 1266.0;      // uncomment this if you want to set the calibration value in the sketch
+  // FoodScale.begin();
+  PetScale.begin();
+  float calibrationValuePetWeight = 22000.0;      // uncomment this if you want to set the calibration value in the sketch
+  float calibrationValue = 1266.0;  
   unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
   boolean _tare = true;                 //set this to false if you don't want tare to be performed in the next step
-  LoadCell.start(stabilizingtime, _tare);
-  if (LoadCell.getTareTimeoutFlag())
+
+  
+  PetScale.start(stabilizingtime, _tare);
+  if (PetScale.getTareTimeoutFlag())
   {
-    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
-    while (1)
-      ;
+    Serial.println("PetScale Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
   }
   else
   {
-    LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
-    Serial.println("Startup is complete");
+    PetScale.setCalFactor(calibrationValuePetWeight); // set calibration value (float)
+    Serial.println("PetScale Startup is complete");
   }
+
+  FoodScale.start(stabilizingtime, _tare);
+  if (FoodScale.getTareTimeoutFlag())
+  {
+    Serial.println("FoodScale Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  }
+  else
+  {
+    FoodScale.setCalFactor(calibrationValue); // set calibration value (float)
+    Serial.println("FoodScale Startup is complete");
+  }
+
 }
 
 void loop()
 {
   server.handleClient();
-  //  Serial.print("Reading: ");
-  //  Serial.print(scale.get_units(), 1); //scale.get_units() returns a float
-  //  Serial.print(" lbs");               //You can change this to kg but you'll need to refactor the calibration_factor
-  //  Serial.println();
-
   if (!signupOK)
   {
     if (Firebase.signUp(&config, &auth, "", ""))
@@ -198,8 +186,8 @@ void loop()
     sendDataPrevMillis = millis();
     String prefix = "/FeedNow/";
     if (Firebase.RTDB.getJSON(&fbdo, prefix + uuid + "/id"))
-    { 
-      bool tmp  = servoActivate;
+    {
+      bool tmp = servoActivate;
       if (fbdo.dataType() == "int")
       {
         int tmpId = fbdo.intData();
@@ -212,37 +200,15 @@ void loop()
               feedAmount = fbdo.intData();
               feednowID = tmpId;
               tmp = true;
-              oldFeed = LoadCell.getData();
+              oldFeed = FoodScale.getData();
             }
           }
         }
       }
       servoActivate = tmp;
     }
-    weight = scale.get_units() * 0.73 / 2.205;
-    if (weight > 0.4)
-    {
-      if (!scaleActivate)
-      {
-        scaleActivateTime = millis();
-        scaleActivate = true;
-      }
-      averageValue.push(weight);
-    }
-    if (averageValue.average() > 0.4 && scaleActivate == true && millis() - scaleActivateTime > 3000)
-    {
-      if (Firebase.RTDB.setFloat(&fbdo, "/PetWeight/" + uuid,averageValue.average())){
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
-    }
-    else {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
-    }
-      scaleActivate = false;
-    }
   }
+
   if (servoActivate)
   {
     myServo.attach(2);
@@ -252,24 +218,52 @@ void loop()
   {
     myServo.detach();
   }
-  static boolean newDataReady = 0;
-  const int serialPrintInterval = 0; //increase value to slow down serial print activity
 
-  // check for new data/start next conversion:
-  if (LoadCell.update())newDataReady = true;
+  // weight = scale.get_units();
+  // if (weight > 0.4)
+  // {
+  //   if (!scaleActivate)
+  //   {
+  //     scaleActivateTime = millis();
+  //     scaleActivate = true;
+  //   }
+  //   averageValue.push(scale.get_units());
+  // }
+  // if (averageValue.average() > 0.4 && scaleActivate == true && millis() - scaleActivateTime > 5000)
+  // {
+  //   Serial.println(averageValue.average());
+  //   if (Firebase.RTDB.setFloat(&fbdo, "/PetWeight/" + uuid, averageValue.average()))
+  //   {
+  //     Serial.println("PASSED");
+  //     Serial.println("PATH: " + fbdo.dataPath());
+  //     Serial.println("TYPE: " + fbdo.dataType());
+  //   }
+  //   else
+  //   {
+  //     Serial.println("FAILED");
+  //     Serial.println("REASON: " + fbdo.errorReason());
+  //   }
+  //   scaleActivate = false;
+  // }
 
   // get smoothed value from the dataset:
-  if (newDataReady)
+  if (PetScale.update() && PetScale.getData() > 0.4 && previousWeight == PetScale.getData())
   {
-    if (millis() > t + serialPrintInterval)
-    {
-      if(LoadCell.getData() > oldFeed + feedAmount){
-        servoActivate = false;
-        Serial.println("servo stop");
-      }
-    }
+     if (Firebase.RTDB.setFloat(&fbdo, "/PetWeight/" + uuid, PetScale.getData()))
+     {
+        Serial.println(PetScale.getData());
+     }
   }
-  
-   
-  }
+  previousWeight = PetScale.getData();
+
+  // get smoothed value from the dataset:
+//  if (FoodScale.update())
+//  {
+//    Serial.printf("Food %f\n",FoodScale.getData());
+//    if (FoodScale.getData() > oldFeed + feedAmount)
+//    {
+//      servoActivate = false;
+//      Serial.println("servo stop");
+//    }
+//  }
 }
